ec15c7d660207282c1b44cba12ea44fc46948795
Evgeny Kudryavtsev <sakhjen@gmail.com>
2025-01-14 21:31:13 +0300

    handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Sakhjen/todo-app"
+	"github.com/Sakhjen/todo-app/pkg/handler"
+)
+
 func main() {
-	
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running server: %s", err.Error())
+
+	}
+
 }
